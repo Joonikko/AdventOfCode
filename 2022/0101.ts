28cca1280557e7ac568a56cbe3b127e7b9298ad4
Joonikko <5886751+Joonikko@users.noreply.github.com>
2022-12-03 15:23:48 +0200

    Add 0102

--- 2022/0101.ts
+++ 2022/0101.ts
@@ -3,7 +3,14 @@ const data = await Deno.readTextFile("./input/0101_data.txt");
 const summed = data
     .split("\n\n")
     .map((l) => l.split("\n"))
-    .map((l) => l.map((num) => parseInt(num)).reduce((prev, cur) => prev + cur, 0));
+    .map((l) => l.map((num) => parseInt(num)).reduce((acc, cur) => acc + cur, 0));
 
 const result = Math.max(...summed);
 console.log(`0101: ${result}`);
+
+const threeLargestSum = [...summed]
+    .sort((a, b) => b - a)
+    .slice(0, 3)
+    .reduce((acc, cur) => acc + cur, 0);
+
+console.log(`0102: ${threeLargestSum}`);
